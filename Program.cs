638effc1fa010bd26bc3fc0eda332fc88d6cfb04
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-10 16:04:28 +0100

    Full plan of application

--- Program.cs
+++ Program.cs
@@ -1,5 +1,27 @@
-﻿
+﻿/*
+ * 
+ * Create a welcome method, a method to ask for the user's name, and another 
+ * to tell that user "Hello <name>", Call these methods from Program.cs.
+ * 
+ */
 
 
 
+//1.Welcome the user(Intro the app name as well)
+    //Ask user for their name
+    //Store the name
 
+
+//2. Ask user for number in party
+    //<convert string to number>
+    //Add the number to the total
+
+
+//3. Check to see if more guests are coming
+    //Loop back to the top until no more guests
+
+
+//4. Loop over the names list and print them
+
+
+//5. Print the total guest count
